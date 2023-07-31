360f0714c9f488cdf12c22c953b2f000a20fed55
alfonsolvg <alfonsolvgcontact@gmail.com>
2023-07-31 13:40:20 -0600

    django channels para tareas asincronas

--- polls/tasks.py
+++ polls/tasks.py
@@ -4,6 +4,8 @@ import random
 import requests
 from celery import shared_task
 from celery.utils.log import get_task_logger
+from celery.signals import task_postrun
+from polls.consumers import notify_channel_layer
 
 logger = get_task_logger(__name__)
 
@@ -14,6 +16,7 @@ def sample_task(email):
 
     api_call(email)
 
+
 @shared_task(bind=True)
 def task_process_notification(self):
     try:
@@ -22,7 +25,16 @@ def task_process_notification(self):
             raise Exception()
 
         # this would block the I/O
-        requests.post('https://httpbin.org/delay/5')
+        requests.post("https://httpbin.org/delay/5")
     except Exception as e:
-        logger.error('exception raised, it would be retry after 5 seconds')
+        logger.error("exception raised, it would be retry after 5 seconds")
         raise self.retry(exc=e, countdown=5)
+
+
+@task_postrun.connect
+def task_postrun_handler(task_id, **kwargs):
+    """
+    When celery task finish, send notification to Django channel_layer, so Django channel would receive
+    the event and then send it to web client
+    """
+    notify_channel_layer(task_id)
--- polls/views.py
+++ polls/views.py
@@ -17,50 +17,55 @@ logger = logging.getLogger(__name__)
 
 # helpers
 
+
 def api_call(email):
     # used for testing a failed api call
     if random.choice([0, 1]):
-        raise Exception('random processing error')
+        raise Exception("random processing error")
 
     # used for simulating a call to a third-party api
-    requests.post('https://httpbin.org/delay/5')
+    requests.post("https://httpbin.org/delay/5")
 
 
 # views
 
+
 def subscribe(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = YourForm(request.POST)
         if form.is_valid():
-            task = sample_task.delay(form.cleaned_data['email'])
+            task = sample_task.delay(form.cleaned_data["email"])
             # return the task id so the JS can poll the state
-            return JsonResponse({
-                'task_id': task.task_id,
-            })
+            return JsonResponse(
+                {
+                    "task_id": task.task_id,
+                }
+            )
 
     form = YourForm()
-    return render(request, 'form.html', {'form': form})
+    return render(request, "form.html", {"form": form})
 
 
 def task_status(request):
-    task_id = request.GET.get('task_id')
+    task_id = request.GET.get("task_id")
 
     if task_id:
         task = AsyncResult(task_id)
         state = task.state
 
-        if state == 'FAILURE':
+        if state == "FAILURE":
             error = str(task.result)
             response = {
-                'state': state,
-                'error': error,
+                "state": state,
+                "error": error,
             }
         else:
             response = {
-                'state': state,
+                "state": state,
             }
         return JsonResponse(response)
-    
+
+
 @csrf_exempt
 def webhook_test(request):
     if not random.choice([0, 1]):
@@ -68,8 +73,9 @@ def webhook_test(request):
         raise Exception()
 
     # blocking process
-    requests.post('https://httpbin.org/delay/5')
-    return HttpResponse('pong')
+    requests.post("https://httpbin.org/delay/5")
+    return HttpResponse("pong")
+
 
 @csrf_exempt
 def webhook_test_async(request):
@@ -78,4 +84,23 @@ def webhook_test_async(request):
     """
     task = task_process_notification.delay()
     logger.info(task.id)
-    return HttpResponse('pong')
+    return HttpResponse("pong")
+
+
+def subscribe_ws(request):
+    """
+    Use Websocket to get notification of Celery task, instead of using ajax polling
+    """
+    if request.method == "POST":
+        form = YourForm(request.POST)
+        if form.is_valid():
+            task = sample_task.delay(form.cleaned_data["email"])
+            # return the task id so the JS can poll the state
+            return JsonResponse(
+                {
+                    "task_id": task.task_id,
+                }
+            )
+
+    form = YourForm()
+    return render(request, "form_ws.html", {"form": form})
